b77120724b8e69329cfcc7f795f6901e49fdf0f4
Feikend <46076900+Feikend@users.noreply.github.com>
2023-06-30 04:17:13 -0600

    more and more fixes

--- res/script.js
+++ res/script.js
@@ -2,8 +2,10 @@ document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
         document.querySelector("body").style.visibility = "hidden";
         document.querySelector("#loader").style.visibility = "visible";
+        document.querySelector("body").style.background = "#654834";
     } else {
         document.querySelector("#loader").style.display = "none";
         document.querySelector("body").style.visibility = "visible";
+        document.querySelector("body").style.background= "";
     }
 };
